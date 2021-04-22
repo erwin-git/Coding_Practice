77ea1599f74269a2787d9a165157712340e25066
Erwin Fratczak <erwin.fratczak@gmail.com>
2021-04-22 11:21:38 +0200

    Exercice5 update

---
+++ exercises/exercise_5/exercise_5.js
@@ -0,0 +1,16 @@
+const img = document.getElementById('img');
+
+fetch('https://api.thecatapi.com/v1/images/search')
+    .then(res => {
+        console.log(res);
+
+        if(res.ok){
+            res.json().then(data => {
+                img.src = data[0].url
+            })
+        } else {
+            console.log("ERROR");
+            document.getElementById('error').innerHTML = "ERROR";
+
+        }
+    })    
